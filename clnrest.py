b279378f3edae072ad3352b525f8e9461831a1b2
ShahanaFarooqui <shahana.farooqui@gmail.com>
2023-06-17 23:24:57 -0700

    Code Refactoring: separated namespace

--- clnrest.py
+++ clnrest.py
@@ -3,29 +3,17 @@
 from gunicorn import glogging
 from gunicorn.workers import sync
 
-import os
-import json5
 from pathlib import Path
-from flask import Flask, request, make_response
-from flask_restx import Api, Namespace, Resource
+from flask import Flask
+from flask_restx import Api
 from gunicorn.app.base import BaseApplication
-from pyln.client import Plugin
 from multiprocessing import Process
 from utilities.generate_certs import generate_certs
-from utilities.shared import call_rpc_method, verify_rune, process_help_response
-
-CERTS_PATH, REST_PROTOCOL, REST_HOST, REST_PORT = "", "", "", ""
-plugin = Plugin()
+from utilities.shared import set_config
+from utilities.rpc_routes import rpcns
+from utilities.rpc_plugin import plugin
 
 jobs = {}
-plugin.add_option(name="rest_certs_path", default=os.getcwd(), description="Path for certificates (for https)", opt_type="string", deprecated=False)
-plugin.add_option(name="rest_protocol", default="https", description="REST server protocol", opt_type="string", deprecated=False)
-plugin.add_option(name="rest_host", default="127.0.0.1", description="REST server host", opt_type="string", deprecated=False)
-plugin.add_option(name="rest_port", default=3010, description="REST server port to listen", opt_type="int", deprecated=False)
-
-methods_list = []
-rpcns = Namespace("RPCs")
-payload_model = rpcns.model("Payload", {}, None, False)
 
 def create_app():
     app = Flask(__name__)
@@ -37,63 +25,8 @@ def create_app():
     api.add_namespace(rpcns, path="/v1")
     return app
 
-@rpcns.route("/list-methods")
-class ListMethodsResource(Resource):
-    @rpcns.response(200, "Success")
-    @rpcns.response(500, "Server error")
-    def get(self):
-        """Get the list of all valid rpc methods"""
-        try:
-            help_response = call_rpc_method(plugin, "help", [])
-            html_content = process_help_response(help_response)
-            response = make_response(html_content)
-            response.headers["Content-Type"] = "text/html"
-            return response
-
-        except Exception as err:
-            plugin.log(f"Error: {err}", "error")
-            return json5.loads(str(err)), 500
-
-@rpcns.route("/<rpc_method>")
-class RpcMethodResource(Resource):
-    @rpcns.doc(security=[{"rune": [], "nodeid": []}])
-    @rpcns.doc(params={"rpc_method": (f"Name of the RPC method to be called")})
-    @rpcns.expect(payload_model, validate=False)
-    @rpcns.response(201, "Success")
-    @rpcns.response(500, "Server error")
-    def post(self, rpc_method):
-        """Call any valid core lightning method (check list-methods response)"""
-        try:
-            is_valid_rune = verify_rune(plugin, request)
-            
-            if "error" in is_valid_rune:
-                plugin.log(f"Error: {is_valid_rune}", "error")
-                raise Exception(is_valid_rune)
-
-        except Exception as err:
-            # Fix Me: Remove after lightningd checkrune is available
-            if not "unknown command" in str(err).lower():
-                return json5.loads(str(err)), 403
-        
-        try:
-            if request.is_json:
-                payload = request.get_json()
-            else:
-                payload = request.form.to_dict()
-            return call_rpc_method(plugin, rpc_method, payload), 201
-
-        except Exception as err:
-            plugin.log(f"Error: {err}", "error")
-            return json5.loads(str(err)), 500
-
-def set_config(options):
-    global CERTS_PATH, REST_PROTOCOL, REST_HOST, REST_PORT
-    CERTS_PATH = str(options["rest_certs_path"])
-    REST_PROTOCOL = str(options["rest_protocol"])
-    REST_HOST = str(options["rest_host"])
-    REST_PORT = int(options["rest_port"])
-
 def set_application_options(plugin):
+    from utilities.shared import CERTS_PATH, REST_PROTOCOL, REST_HOST, REST_PORT
     plugin.log(f"Server is starting at {REST_PROTOCOL}://{REST_HOST}:{REST_PORT}", "info")
     if REST_PROTOCOL == "http":
         options = {
@@ -111,7 +44,6 @@ def set_application_options(plugin):
             plugin.log(f"Certs Path: {CERTS_PATH}", "info")
         except Exception as err:
             raise Exception(f"{err}: Certificates do not exist at {CERTS_PATH}")
-
         options = {
             "bind": f"{REST_HOST}:{REST_PORT}",
             "workers": 1,
@@ -141,7 +73,8 @@ def worker():
     app = create_app()
     CLNRestApplication(app, options).run()
 
-def start_server(REST_PORT):
+def start_server():
+    from utilities.shared import REST_PORT
     if REST_PORT in jobs:
         return False, "server already running"
 
@@ -158,6 +91,6 @@ def start_server(REST_PORT):
 @plugin.init()
 def init(options, configuration, plugin):
     set_config(options)
-    start_server(REST_PORT)
+    start_server()
 
 plugin.run()
